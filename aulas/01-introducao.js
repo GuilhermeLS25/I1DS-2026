995038ef96f5d25f330c57f08e86c21ebec6a1bc
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-17 10:53:42 -0300

    .

--- aulas/01-introducao.js
+++
@@ -1,8 +0,0 @@
-// Escrevendo no console - console.log();
-console.log("Olá, eu sou um texto!");
-console.log("E eu sou outro texto!\n");
-
-console.log("Escrevendo alguns dados:");
-console.log(17);
-console.log(17,"anos");
-console.log(26.31);
