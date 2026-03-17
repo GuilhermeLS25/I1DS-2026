d6e064dcae44daf722c89e29555cf21f8dc13ad5
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-17 08:51:01 -0300

    Adiciona exemplos de operadores lógicos e concatenação de strings em JavaScript

--- aulas/05-Operadores Logicos.js
+++ aulas/05-Operadores Logicos.js
@@ -1,12 +1,28 @@
-let soupobre = true;
+// Operadores Lógicos
+let souPobre = true;
 
-console.log("Sou pobre?", soupobre);
-console.log("Negação de soupobre", !soupobre);
+console.log("Sou pobre?", souPobre);
+console.log("Negação de souPobre:", !souPobre);
 
 let manha = true;
 let sono = true;
+console.log("-------------------------");
+console.log("Manhã:", manha, "| Estou com sono?", sono);
 
-console.log("-------------------------------");
+// Comparador E (AND) e OU (OR)
+console.log("Operador AND (E):", manha && sono);
+console.log("Operador OR (OU):", manha || sono);
+
+manha = false;
+console.log("-------------------------");
+console.log("Manhã:", manha, "| Estou com sono?", sono);
+
+// Comparador E (AND) e OU (OR)
+console.log("Operador AND (E):", manha && sono);
+console.log("Operador OR (OU):", manha || sono);
+
+sono = false;
+console.log("-------------------------");
 console.log("Manhã:", manha, "| Estou com sono?", sono);
 
 // Comparador E (AND) e OU (OR)
