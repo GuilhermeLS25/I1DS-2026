c0ab1ec436639f9a7eb36bd4e5d8afb9f2a160c0
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-17 10:58:44 -0300

    Criando e Imprimindo variaveis do tipo Array (Vetor)

--- aulas/08-vetor.js
+++ aulas/08-vetor.js
@@ -2,5 +2,11 @@
 
 var dinos = ["Tiranosauro Rex", "Estegossauro", "Brontossauro"];
 
+// Imprimindo os dados em forma de linha
 console.log(dinos);
+
+// Imprimindo os dados em forma de tabela
 console.table(dinos);
+
+// lenght -> "Tamanho" do Array (Quantidade de Elementos)
+console.log("O vetor tem", dinos.length, "elementos");
