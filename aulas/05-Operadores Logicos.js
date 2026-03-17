846b86581c3b8e6d181d6fe53cd1766d43a4f0e7
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-17 08:49:02 -0300

    Adiciona exercícios de fixação e demonstrações de variáveis, operações aritméticas e operadores lógicos em JavaScript

---
+++ aulas/05-Operadores Logicos.js
@@ -0,0 +1,14 @@
+let soupobre = true;
+
+console.log("Sou pobre?", soupobre);
+console.log("Negação de soupobre", !soupobre);
+
+let manha = true;
+let sono = true;
+
+console.log("-------------------------------");
+console.log("Manhã:", manha, "| Estou com sono?", sono);
+
+// Comparador E (AND) e OU (OR)
+console.log("Operador AND (E):", manha && sono);
+console.log("Operador OR (OU):", manha || sono);
