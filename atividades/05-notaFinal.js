1dd4deb31159da5b10e8db61a100818ccc0be24b
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-18 11:29:37 -0300

    .

---
+++ atividades/05-notaFinal.js
@@ -0,0 +1,44 @@
+/*****************************************************
+Lista de Exercícios
+****************************************************/
+/*
+Crie uma função que receba a nota de um aluno e
+retorne uma mensagem informando sua situação.
+
+
+    Considere as seguintes regras:
+
+
+        Nota menor que 5: reprovado
+        Nota entre 5 e 6.9: recuperação
+        Nota igual ou maior que 7: aprovado
+
+
+    A função deve retornar uma frase indicando a situação do aluno.
+*/
+
+// Verificação de aprovação
+
+function vericacao(nota) {
+  let frase;
+  if (nota < 5) {
+    frase = "REPROVADO!!";
+  } else if (nota > 5 && nota < 6.9) {
+    frase = "Recuperação";
+  } else {
+    frase = "APROVADO!!";
+  }
+  return frase;
+}
+
+let nota = 8;
+console.log("Você está: ", vericacao(nota));
+
+// Média Final
+
+let n1 = 8
+let n2 = 7
+let n3 = 5
+let n4 = 2
+
+
