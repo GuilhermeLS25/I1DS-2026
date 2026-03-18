c261070cda011bd24441946fc185b5e556484e6d
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-18 16:01:07 -0300

    .

---
+++ atividades/07-SistemaMediaEscolar.js
@@ -0,0 +1,20 @@
+/* 
+🧠 Desafio de Programação – Sistema de Média Escolar
+🎯 Objetivo
+
+
+Criar um programa em JavaScript que calcule a média final de um aluno e informe sua situação no final do ano.
+
+
+📌 Regras do programa
+1. O programa deve receber 4 notas (de 0 a 10).
+2. Calcular a média das notas.
+3. Informar a situação do aluno de acordo com a média:
+
+
+Média                   Situação
+menor que 5             Reprovado
+entre 5 e 6.9           Recuperação
+maior ou igual a 7      Aprovado
+*/
+
