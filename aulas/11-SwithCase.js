b1db7248c7b54770851be645f74243899e10fcc5
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-18 16:04:25 -0300

    .

--- aulas/11-SwithCase.js
+++ aulas/11-SwithCase.js
@@ -2,7 +2,6 @@
 // uma variável com vários possíveis resultados.
 // REGRA DO PROFCASTELLO - se tiver mais que 3 comparadores, faça um switch case
 
-
 // Exemplo 01 - MENU
 // Neste exemplo a variável menuSelecionado guardará o nome de uma opção de menu.
 // O switch verifica qual foi a opção escolhida e exibe uma mensagem correspondente
