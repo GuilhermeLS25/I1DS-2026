5d75dd47588807112f605d8b8e513ab7e729c177
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-18 10:09:00 -0300

    .

---
+++ aulas/10-DesvioCondicional.js
@@ -0,0 +1,17 @@
+// Desvio Condicional - IF/ELSE
+
+/* Condições simples com IF
+Sintaxe: if (condição)(
+    codigo a ser executada, se verdadeiro
+)
+*/
+
+let anoNascimento = 2002;
+
+if (anoNascimento > 2003) {
+  console.log("A pessoa nasceu depois de 2003");
+}
+
+if (anoNascimento < 2003) {
+  console.log("A pessoa nasceu antes de 2003");
+}
