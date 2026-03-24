d936e2de305f4dd080aca60f4bb74e556d13f43b
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-24 14:54:58 -0300

    .

---
+++ aulas/08-matriz.js
@@ -0,0 +1,28 @@
+// Criando uma matriz
+var matriz = [
+    [9, 0, 1]
+    [7, 1, 2]
+    [6, 2, 8]
+    [1, 3, 9]
+]
+console.table(matriz);
+
+// Obtendo elemento com base e seus índices
+
+console.log(matriz[(0, 1)]);
+
+/*********************************************
+                exercício
+*********************************************/
+/*
+    1. crie uma matriz 3x3
+    2. Imprime os valores da diagonal principal X
+    3. Altere os valores do item matriz[1][2] para 20 e
+       matriz[2][0] para 30
+*/
+let matris = [
+    [9, 0, 1]
+    [7, 1, 2]
+    [6, 2, 8]
+];
+console.table(matris);
