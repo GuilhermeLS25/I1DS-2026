dba7a3d76a18f71aade37173f067bc4b5906d62e
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-25 10:43:12 -0300

    1. Crie variáveis para nome, idade e cidade. Exiba uma frase completa.

---
+++ 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/01-nome_idade_cidade.js
@@ -0,0 +1,7 @@
+let nome = "Guilherme";
+let idade = 17;
+let cidade = "Jaú";
+
+console.log("================================");
+console.log(" -> Seu nome é:", nome,"\n -> Você tem:", idade,"anos","\n -> Você mora em:", cidade);
+console.log("================================");
