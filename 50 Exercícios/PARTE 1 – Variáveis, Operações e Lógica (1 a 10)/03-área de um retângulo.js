2e05576e2c66ed5a25d2347c3f202cd8568e2bb4
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-25 15:13:53 -0300

    .

--- 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/03-área de um retângulo.js
+++ 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/03-área de um retângulo.js
@@ -4,3 +4,7 @@ let altura = 4
 console.log("--------------------------------------------------------")
 console.log("3. Calcule a área de um retângulo.")
 console.log("--------------------------------------------------------")
+console.log("A base do triangulo mede:",base)
+console.log("A altura do triangulo mede:", altura)
+console.log("--------------------------------------------------------")
+let area = (base * altura) / 2
