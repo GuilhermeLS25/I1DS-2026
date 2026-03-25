315810a8b2fa046b4951d5c54b0ce6ccdbe98561
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-25 10:28:17 -0300

    .

--- atividades/08-atidade.js
+++ atividades/08-atidade.js
@@ -14,7 +14,11 @@ let matriz = [
   [1, 9, 2],
   [7, 1, 4],
 ];
-matriz;
+
+console.log("***************************************************************");
+console.log("Dada a matriz abaixo, faça um programa em JavaScript\nque imprima os elementos da sua diagonal principal.\n| 3 5 8 |\n| 1 9 2 |\n| 7 1 4 |");
+console.log("***************************************************************");
+
 for (let i = 0; i < 3; i++) {
   console.table(matriz[i][i]);
 }
@@ -29,16 +33,18 @@ let matriz2 = [
   [2, 9, 1],
   [4, 6, 2],
 ];
-let x = 2
+let x = 2;
 
-console.log("****************************************************")
-console.log("Dada a matriz abaixo, faça um programa em JavaScript\nque multiplique seus elementos por um valor x e imprima\na matriz antes e após a multiplicação.")
-console.log("****************************************************")
+console.log("****************************************************");
+console.log(
+  "Dada a matriz abaixo, faça um programa em JavaScript\nque multiplique seus elementos por um valor x e imprima\na matriz antes e após a multiplicação.",
+);
+console.log("****************************************************");
 
 console.table(matriz2);
 for (let i = 0; i < matriz2.length; i++) {
   for (let j = 0; j < matriz2[i].length; j++) {
-    matriz2[i][j] *= x
+    matriz2[i][j] *= x;
   }
 }
 
