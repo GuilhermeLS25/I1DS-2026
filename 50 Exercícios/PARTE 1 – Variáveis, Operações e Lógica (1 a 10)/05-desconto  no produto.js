3e935c577f098099073283a1ed8717dc91751f15
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-25 16:28:58 -0300

    .

---
+++ 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/05-desconto  no produto.js
@@ -0,0 +1,9 @@
+let p = 7
+let desconto = (p * 10) / 100
+
+console.log("---------------------------------------------------------------------")
+console.log("5. Dado o valor de um produto, calcule: 10% de desconto | valor final")
+console.log("---------------------------------------------------------------------")
+console.log("Valor do Produto:", p)
+console.log("Valor de Desconto: 10%")
+console.log("O valor final")
