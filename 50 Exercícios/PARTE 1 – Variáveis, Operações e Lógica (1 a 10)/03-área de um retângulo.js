8b79ac365ef8efcbd75cce5173f1d44c4a33a8f0
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-25 11:18:14 -0300

    3. Calcule a área de um retângulo.

---
+++ 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/03-área de um retângulo.js
@@ -0,0 +1,6 @@
+let base = 2
+let altura = 4
+
+console.log("--------------------------------------------------------")
+console.log("3. Calcule a área de um retângulo.")
+console.log("--------------------------------------------------------")
