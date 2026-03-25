3b78e1e2eaaa15b9443f6105e22e06b68fe85b12
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-25 15:47:55 -0300

    3. Calcule a área de um retângulo.

--- 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/03-área de um retângulo.js
+++ 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/03-área de um retângulo.js
@@ -1,10 +1,13 @@
-let base = 2
-let altura = 4
+let base = 5;
+let altura = 9;
+let area = (base * altura) / 2;
 
-console.log("--------------------------------------------------------")
-console.log("3. Calcule a área de um retângulo.")
-console.log("--------------------------------------------------------")
-console.log("A base do triangulo mede:",base)
-console.log("A altura do triangulo mede:", altura)
-console.log("--------------------------------------------------------")
-let area = (base * altura) / 2
+
+console.log("--------------------------------------------------------");
+console.log("3. Calcule a área de um retângulo.");
+console.log("--------------------------------------------------------");
+console.log("A base do triangulo mede:", base);
+console.log("A altura do triangulo mede:", altura);
+console.log("--------------------------------------------------------");
+console.log("Logo, area total do triangulo é igual a:", area);
+console.log("--------------------------------------------------------");
