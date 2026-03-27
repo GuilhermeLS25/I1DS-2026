77ec07c7e545df950321b502c57edb4dcebbeb19
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-27 19:31:03 -0300

    23. Dado um mês, informe o trimestre.

--- 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/PARTE 3 – Switch Case (21 a 25)/23-Trimetre.js
+++ 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/PARTE 3 – Switch Case (21 a 25)/23-Trimetre.js
@@ -1,10 +1,36 @@
 let mes = 2;
 
-switch (mes)) {
-    case (1 || 2 || 3):
-        
+switch (mes) {
+    case 1:
+    case 2:
+    case 3:
+        console.log("----------------------------------------");
+        console.log("> 23.Dado um mês, informe o trimestre. <");
+        console.log("----------------------------------------");
+        console.log("-> Você está no Primeiro(1º), Trimestre!");
+        console.log("----------------------------------------");
+        break
+    case 4:
+    case 5:
+    case 6:
+        console.log("----------------------------------------");
+        console.log("> 23.Dado um mês, informe o trimestre. <");
+        console.log("----------------------------------------");
+        console.log("-> Você está no Segundo(2º), Trimestre!");
+        console.log("---------------------------------------");
         break;
-
+    case 7:
+    case 8:
+    case 9:
+        console.log("----------------------------------------");
+        console.log("> 23.Dado um mês, informe o trimestre. <");
+        console.log("----------------------------------------");
+        console.log("-> Você está no Terceiro(3º), Trimestre!");
+        console.log("----------------------------------------");
+        break
     default:
+        console.log("----------------------------");
+        console.log("-> Trimestre NÃO Existente!!");
+        console.log("----------------------------");
         break;
 }
