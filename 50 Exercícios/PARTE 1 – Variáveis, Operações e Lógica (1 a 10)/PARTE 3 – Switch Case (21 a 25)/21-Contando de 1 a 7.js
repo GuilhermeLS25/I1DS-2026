2d73121ce687a329540c70ebf0f2545b5ef01f86
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-27 17:48:49 -0300

    .

--- 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/PARTE 3 – Switch Case (21 a 25)/21-Contando de 1 a 7.js
+++ 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/PARTE 3 – Switch Case (21 a 25)/21-Contando de 1 a 7.js
@@ -1,9 +1,20 @@
-let num = 0;
-switch (key) {
-    case value:
-        
-        break;
-
+let num = 4;
+switch (num) {
+    case 1:
+        console.log("Segunda-Feira");
+    case 2:
+        console.log("Terça-Feira");
+    case 3:
+        console.log("Quarta-Feira");
+    case 4:
+        console.log("Quinta-Feira");
+    case 5:
+        console.log("Sexta-Feira");
+    case 6:
+        console.log("Sabádo-Feira");
+    case 7:
+        console.log("Domingo-Feira");
     default:
+        console.log("Dia da Semana NÂO Existente!!");
         break;
 }
