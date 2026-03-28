178ca0e78012a39a7096134f812aa3286cc63ba9
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 20:30:41 -0300

    34. Exiba todos os múltiplos de 5 entre 0 e 100.

--- Lista de Execícios - JavaScript/PARTE 4 – Laço FOR (26 a 35) 🚨 (TODOS DEVEM USAR for)/34-multiplosD5.js
+++ Lista de Execícios - JavaScript/PARTE 4 – Laço FOR (26 a 35) 🚨 (TODOS DEVEM USAR for)/34-multiplosD5.js
@@ -1,9 +1,14 @@
 //34. Exiba todos os múltiplos de 5 entre 0 e 100.
 
-let j = 8;
+let j = 5;
+
+console.log("Vai COMEÇAR!!...");
 
 for (let i = 0; i <= 100 ; i++) {
     if (i % j == 0 ) {
         console.log(i);
     }    
 }
+
+console.log("FIM!!");
+
