916a699a9f0dc50540014546c2c57c3f47da70e5
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 12:40:06 -0300

    14. Verifique se um aluno foi: Aprovado (≥ 7) / Recuperação (≥ 5 e < 7) / Reprovado (< 5).

---
+++ 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/PARTE 2 – Condicionais (IF) (11 a 20)/14-NotaAluno.js
@@ -0,0 +1,27 @@
+/**********************************
+14. Verifique se um aluno foi:
+       Aprovado (≥ 7)
+       Recuperação (≥ 5 e < 7)
+       Reprovado (< 5)
+       
+**********************************/
+let nota = 8;
+
+if (nota >= 7) {
+    console.log("------------------------");
+    console.log("-> O aluno foi APROVADO!");
+    console.log("------------------------");
+
+} else if (nota >= 5 && nota < 7) {
+
+    console.log("-------------------------------");
+    console.log("-> O aluno está de RECUPERAÇÃO!");
+    console.log("-------------------------------");
+
+} else {
+
+    console.log("--------------------------");
+    console.log("-> O aluno está REPROVADO!");
+    console.log("--------------------------");
+
+}
