0ec51c3fa026f9bbd51847479dfeb98e16fd10c0
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 20:14:31 -0300

    14. Verifique se um aluno foi: Aprovado (≥ 7) / Recuperação (≥ 5 e < 7) / Reprovado (< 5)

--- Lista de Execícios - JavaScript/PARTE 2 – Condicionais (IF) (11 a 20)/14-NotaAluno.js
+++ Lista de Execícios - JavaScript/PARTE 2 – Condicionais (IF) (11 a 20)/14-NotaAluno.js
@@ -5,9 +5,11 @@
        Reprovado (< 5)
        
 **********************************/
+
 let nota = 8;
 
 if (nota >= 7) {
+    
     console.log("------------------------");
     console.log("-> O aluno foi APROVADO!");
     console.log("------------------------");
