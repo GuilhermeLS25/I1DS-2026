416cdcc45f102d72242b6876a16fb7f616474c28
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 13:39:54 -0300

    20. Classifique a idade: Criança, Adolescente, Adulto, Idoso.

---
+++ 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/PARTE 2 – Condicionais (IF) (11 a 20)/20-classificacaoIdade.js
@@ -0,0 +1,35 @@
+/*************************
+20. Classifique a idade:
+    -Criança
+    -Adolescente
+    -Adulto
+    -Idoso
+**************************/
+
+let idade = 18;
+
+if (idade >= 60) {
+
+    console.log("------------------------------------");
+    console.log("-> Você é classificado como: IDOSO!!");
+    console.log("------------------------------------");
+
+} else if (idade >= 18) {
+
+    console.log("-------------------------------------");
+    console.log("-> Você é classificado como: ADULTO!!");
+    console.log("-------------------------------------");
+
+} else if (idade >= 12) {
+
+    console.log("------------------------------------------");
+    console.log("-> Você é classificado como: ABORRECENTE!!");
+    console.log("------------------------------------------");
+
+} else {
+
+    console.log("--------------------------------------");
+    console.log("-> Você é classificado como: CRIANÇA!!");
+    console.log("--------------------------------------");
+
+}
