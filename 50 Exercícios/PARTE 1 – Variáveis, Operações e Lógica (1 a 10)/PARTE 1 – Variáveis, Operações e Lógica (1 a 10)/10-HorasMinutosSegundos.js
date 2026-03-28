4c9d6322068eb19efd3141a0dced736a5b1ac2d8
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 16:51:30 -0300

    10. Dado um valor em horas, converta para minutos e segundos.

---
+++ 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/10-HorasMinutosSegundos.js
@@ -0,0 +1,11 @@
+//10. Dado um valor em horas, converta para minutos e segundos.
+
+let horas = 4
+let minutos = (horas * 60)
+let segundos = (horas * 3600)
+
+console.log("----------------------------");
+ console.log("Horas:", horas,);
+ console.log("Minutos:", minutos);
+console.log("Segundos:", segundos);
+console.log("----------------------------");
