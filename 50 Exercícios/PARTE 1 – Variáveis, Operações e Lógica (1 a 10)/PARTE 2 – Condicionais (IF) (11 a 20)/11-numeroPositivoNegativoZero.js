4d1a3b6ecf1d35b493ef2160b067c5c824b2d7ce
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 12:15:23 -0300

    11. Verifique se um número é positivo, negativo ou zero.

---
+++ 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/PARTE 2 – Condicionais (IF) (11 a 20)/11-numeroPositivoNegativoZero.js
@@ -0,0 +1,20 @@
+let numero = 0;
+
+if (numero > 0) {
+    
+    console.log("-----------------------");
+    console.log("-> O número é positivo.");
+    console.log("-----------------------");
+
+} else if (numero < 0) {
+    
+    console.log("-----------------------");
+    console.log("-> O número é negativo.");
+    console.log("-----------------------");
+
+} else {
+    
+    console.log("-------------------");
+    console.log("-> O número é zero.");
+    console.log("-------------------");
+}
