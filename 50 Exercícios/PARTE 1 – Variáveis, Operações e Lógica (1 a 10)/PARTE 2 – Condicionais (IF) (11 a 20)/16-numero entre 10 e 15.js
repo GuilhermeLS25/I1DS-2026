3934c4eb31db7c1c058f77f5328757f8b5e41ac1
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 13:04:37 -0300

    16. Verifique se um número está entre 10 e 50.

---
+++ 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/PARTE 2 – Condicionais (IF) (11 a 20)/16-numero entre 10 e 15.js
@@ -0,0 +1,16 @@
+//16. Verifique se um número está entre 10 e 50.
+
+let num = 50;
+
+if (num >= 10 && num <= 50){
+    
+    console.log("-----------------------------------------");
+    console.log("-> O número", num, "ESTÁ entre 10 e 50!");
+    console.log("-----------------------------------------");
+
+} else {
+    console.log("--------------------------------------------");
+    console.log("-> o número", num, "NÃO ESTÁ entre 10 e 50");
+    console.log("--------------------------------------------");
+
+}
