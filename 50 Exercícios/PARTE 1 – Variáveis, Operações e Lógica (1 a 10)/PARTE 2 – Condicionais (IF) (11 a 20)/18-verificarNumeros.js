67688a7be21e41e4f1d24612229ea6b66bc67e9b
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 13:17:56 -0300

    18. Verifique o maior entre dois números.

---
+++ 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/PARTE 2 – Condicionais (IF) (11 a 20)/18-verificarNumeros.js
@@ -0,0 +1,18 @@
+//18. Verifique o maior entre dois números.
+
+let num1 = 10;
+let num2 = 8;
+
+if (num1 > num2){
+
+    console.log("------------------------------------------");
+    console.log("-> O número", num1, "é maior que", num2);
+    console.log("------------------------------------------");
+
+} else {
+
+    console.log("------------------------------------------");
+    console.log("-> O número", num2, "é maior que", num1);
+    console.log("------------------------------------------");
+
+}
