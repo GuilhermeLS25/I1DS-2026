0afda6db95f9fd94b8d34768ae2b6f766f62c54f
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 20:13:58 -0300

    13. Dado um número, verifique se é múltiplo de 3.

--- Lista de Execícios - JavaScript/PARTE 2 – Condicionais (IF) (11 a 20)/13-multiplo de 3.js
+++ Lista de Execícios - JavaScript/PARTE 2 – Condicionais (IF) (11 a 20)/13-multiplo de 3.js
@@ -1,11 +1,15 @@
+//13. Dado um número, verifique se é múltiplo de 3.
+
 let num = 15;
 
 if (num % 3 == 0) {
+
     console.log("---------------------------------------");
     console.log("-> O número", num, ",é múltiplo de 3!");
     console.log("---------------------------------------");
 
 } else {
+
     console.log("-------------------------------------------");
     console.log("-> O número", num, ",não é múltiplo de 3!");
     console.log("-------------------------------------------");
