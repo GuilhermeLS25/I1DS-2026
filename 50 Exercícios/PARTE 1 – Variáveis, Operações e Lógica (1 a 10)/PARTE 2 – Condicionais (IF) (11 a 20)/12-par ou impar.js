6975da2d09f2f96771cf921b940a4b72f2765642
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 12:18:51 -0300

    12. Verifique se um número é par ou ímpar.

---
+++ 50 Exercícios/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/PARTE 2 – Condicionais (IF) (11 a 20)/12-par ou impar.js
@@ -0,0 +1,17 @@
+//12. Verifique se um número é par ou ímpar.
+
+let num = 10;
+
+if (num % 2 == 0) {
+
+    console.log("---------------------");
+    console.log("-> Este número é par!");
+    console.log("---------------------");
+
+} else {
+    
+    console.log("-----------------------");
+    console.log("-> Este número é impar!");
+    console.log("-----------------------");
+
+}
