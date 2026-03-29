a94414107b6af9fcd973e90d22a13c2a9c343bae
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 21:42:56 -0300

    5. Dado o valor de um produto, calcule: 10% de desconto | valor final

--- Lista de Execícios - JavaScript/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/05-desconto  no produto.js
+++ Lista de Execícios - JavaScript/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/05-desconto  no produto.js
@@ -4,18 +4,10 @@ let p = 7;
 let d = (p * 10) / 100;
 let f = p - d;
 
-console.log(
-  "---------------------------------------------------------------------",
-);
-console.log(
-  "5. Dado o valor de um produto, calcule: 10% de desconto | valor final",
-);
-console.log(
-  "---------------------------------------------------------------------",
-);
+console.log("---------------------------------------------------------------------");
+console.log("5. Dado o valor de um produto, calcule: 10% de desconto | valor final",);
+console.log("---------------------------------------------------------------------");
 console.log("Valor do Produto: R$", p);
 console.log("Valor do Desconto: 10%");
 console.log("Valor final: R$", f);
-console.log(
-  "---------------------------------------------------------------------",
-);
+console.log("---------------------------------------------------------------------");
