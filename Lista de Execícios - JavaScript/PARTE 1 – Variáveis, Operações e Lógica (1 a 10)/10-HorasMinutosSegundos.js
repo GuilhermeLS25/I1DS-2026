7b93fab28b0408ad6efce12bcfff186ec23741ae
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 21:43:16 -0300

    10. Dado um valor em horas, converta para minutos e segundos.

--- Lista de Execícios - JavaScript/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/10-HorasMinutosSegundos.js
+++ Lista de Execícios - JavaScript/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/10-HorasMinutosSegundos.js
@@ -1,8 +1,8 @@
 //10. Dado um valor em horas, converta para minutos e segundos.
 
-let horas = 4
-let minutos = (horas * 60)
-let segundos = (horas * 3600)
+let horas = 4;
+let minutos = (horas * 60);
+let segundos = (horas * 3600);
 
 console.log("----------------------------");
  console.log("Horas:", horas,);
