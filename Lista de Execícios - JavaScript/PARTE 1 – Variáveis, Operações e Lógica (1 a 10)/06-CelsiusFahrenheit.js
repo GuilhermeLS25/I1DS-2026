001730fb034bf348f3d89d459761c8a2702a74cd
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-29 15:11:48 -0300

    6. Converta temperatura de Celsius para Fahrenheit.

--- Lista de Execícios - JavaScript/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/06-CelsiusFahrenheit.js
+++ Lista de Execícios - JavaScript/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/06-CelsiusFahrenheit.js
@@ -3,4 +3,6 @@
 let c = 32;
 let f = c * 1.8 + 32;
 
-console.log("A temperatura de", c, "graus celcius em fahrenheit é igual a", f);
+console.log("-----------------------------------------------------------------");
+console.log("-> A temperatura de", c, "graus celcius em fahrenheit é igual a", f);
+console.log("-----------------------------------------------------------------");
