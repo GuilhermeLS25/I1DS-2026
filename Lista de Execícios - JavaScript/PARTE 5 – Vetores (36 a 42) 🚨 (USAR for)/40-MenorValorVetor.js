61d3da6a1f16065ec51d8d207a809801f4fa9bf4
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 21:45:43 -0300

    40. Encontre o menor valor do vetor.

---
+++ Lista de Execícios - JavaScript/PARTE 5 – Vetores (36 a 42) 🚨 (USAR for)/40-MenorValorVetor.js
@@ -0,0 +1,12 @@
+//40. Encontre o menor valor do vetor.
+
+var num = [5, 8, 9, 4, 7, 1, 10];
+let menor = num[0];
+
+for (let i = 0; i < num.length; i++) {
+    if (num[i] < menor) {
+        menor = num[i];
+    }
+}
+
+console.log("O número", menor, "é o menor valor do Vetor!!");
