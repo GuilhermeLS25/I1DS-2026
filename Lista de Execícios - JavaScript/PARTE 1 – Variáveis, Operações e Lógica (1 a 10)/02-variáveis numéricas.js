15551812a6e989791b23919d27a870aa007593d7
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 21:42:47 -0300

    2. Crie duas variáveis numéricas e exiba: soma, subtração, multiplicação, divisão, resto da divisão

--- Lista de Execícios - JavaScript/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/02-variáveis numéricas.js
+++ Lista de Execícios - JavaScript/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/02-variáveis numéricas.js
@@ -11,10 +11,10 @@ let resto = num1 % num2;
 
 
 console.log("-----------------------------------------------------------------");
-console.log("2. Crie duas variáveis numéricas e exiba:\nsoma, subtração, multiplicação, divisão, resto da divisão")
+console.log("2. Crie duas variáveis numéricas e exiba:\nsoma, subtração, multiplicação, divisão, resto da divisão");
 console.log("------------------------------------------------------------------");
 console.log("1º Número:", num1);
-console.log("2º Número:", num2)
+console.log("2º Número:", num2);
 console.log("------------------------------------------------------------------");
 
 console.log(" -> A soma entre", num1,"e", num2,"é igual a", soma);
