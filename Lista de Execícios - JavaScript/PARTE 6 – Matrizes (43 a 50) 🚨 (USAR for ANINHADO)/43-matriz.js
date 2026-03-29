7be03117db7f7790824ee0b146b47f6d6c2c3108
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-29 15:13:46 -0300

    43. Crie uma matriz 3x3 e exiba todos os valores.

--- Lista de Execícios - JavaScript/PARTE 6 – Matrizes (43 a 50) 🚨 (USAR for ANINHADO)/43-matriz.js
+++ Lista de Execícios - JavaScript/PARTE 6 – Matrizes (43 a 50) 🚨 (USAR for ANINHADO)/43-matriz.js
@@ -6,6 +6,8 @@ var matriz = [
     [7, 8, 9],
 ];
 
+console.table(matriz);
+
 for (let i = 0; i < matriz.length; i++) {
     for (let j = 0; j < matriz[i].length; j++) {
         console.log("Posição [", i, ",", j, "] =", matriz[i][j]);
