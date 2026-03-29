590623b1e1d74c533bf8d08cbec37a5c13930bf7
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-29 15:12:05 -0300

    8. Calcule a média de 3 notas.

--- Lista de Execícios - JavaScript/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/08-mediaNotas.js
+++ Lista de Execícios - JavaScript/PARTE 1 – Variáveis, Operações e Lógica (1 a 10)/08-mediaNotas.js
@@ -7,8 +7,8 @@ let n3 = 9;
 let media = (n1 + n2 + n3) / 4;
 
 console.log("------------------------------");
-console.log("Nota da primeira prova:", n1);
-console.log("Nota da segunda prova:", n2);
-console.log("Nota da terceira prova:", n3);
+console.log("Nota da primeira, 1º prova:", n1);
+console.log("Nota da segunda, 2º prova:", n2);
+console.log("Nota da terceira, 3º prova:", n3);
 console.log("A sua média é:", media.toFixed(2));
 console.log("------------------------------");
