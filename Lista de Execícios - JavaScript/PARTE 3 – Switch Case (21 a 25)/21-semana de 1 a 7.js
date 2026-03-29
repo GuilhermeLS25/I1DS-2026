a6f7eb9ef83ffd9dd085e276cf4e908890f54855
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-29 15:12:23 -0300

    21. Dado um número de 1 a 7, exiba o dia da semana.

--- Lista de Execícios - JavaScript/PARTE 3 – Switch Case (21 a 25)/21-semana de 1 a 7.js
+++ Lista de Execícios - JavaScript/PARTE 3 – Switch Case (21 a 25)/21-semana de 1 a 7.js
@@ -4,27 +4,43 @@ let num = 3;
 
 switch (num) {
     case 1:
+        console.log("-------------");
         console.log("Segunda-Feira");
+        console.log("-------------");
         break
     case 2:
+        console.log("-----------");
         console.log("Terça-Feira");
+        console.log("-----------");
         break
     case 3:
+        console.log("------------");
         console.log("Quarta-Feira");
+        console.log("------------");
         break
     case 4:
+        console.log("------------");
         console.log("Quinta-Feira");
+        console.log("------------");
         break
     case 5:
+        console.log("-----------");
         console.log("Sexta-Feira");
+        console.log("-----------");
         break
     case 6:
+        console.log("------------");
         console.log("Sabádo-Feira");
+        console.log("------------");
         break
     case 7:
+        console.log("-------------");
         console.log("Domingo-Feira");
+        console.log("-------------");
         break
     default:
+        console.log("-----------------------------");
         console.log("Dia da Semana NÂO Existente!!");
+        console.log("-----------------------------");
         break;
 }
