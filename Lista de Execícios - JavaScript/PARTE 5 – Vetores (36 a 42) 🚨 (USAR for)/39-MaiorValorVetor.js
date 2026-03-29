4955c5dac5d33d21d465fb6f9ab63a6b2333ac7f
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-28 21:45:37 -0300

    39. Encontre o maior valor do vetor.

---
+++ Lista de Execícios - JavaScript/PARTE 5 – Vetores (36 a 42) 🚨 (USAR for)/39-MaiorValorVetor.js
@@ -0,0 +1,12 @@
+//39. Encontre o maior valor do vetor.
+
+var num = [5, 6, 9, 7, 4, 1];
+let maior = num[0];
+
+for (let i = 0; i < num.length; i++) {
+    if (num[i] > maior) {
+        maior = num[i];
+    }
+}
+
+console.log("-> O número", maior, "é maior que todos os outros números!");
