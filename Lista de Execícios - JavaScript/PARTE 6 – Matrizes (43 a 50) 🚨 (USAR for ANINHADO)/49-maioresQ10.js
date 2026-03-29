5f6e6db4fd437dc2ce425cc9cfe6806f24d58da1
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-03-29 13:30:51 -0300

    49. Conte quantos valores são maiores que 10.

---
+++ Lista de Execícios - JavaScript/PARTE 6 – Matrizes (43 a 50) 🚨 (USAR for ANINHADO)/49-maioresQ10.js
@@ -0,0 +1,19 @@
+//49. Conte quantos valores são maiores que 10.
+
+var matriz = [
+    [2, 4, 6],
+    [8, 10, 12],
+    [14, 16, 18],
+];
+console.table(matriz);
+let contador = 0;
+
+for (let l = 0; l < matriz.length; l++) {
+    for (let c = 0; c < matriz[l].length; c++) {
+        if (matriz[l][c] > 10) {
+            contador++;
+        }
+    }
+}
+
+console.log("O total de Números que são maiores do que 10 é:", contador);
