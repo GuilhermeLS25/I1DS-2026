9ed3dde876951397aa4a0cdcdd189c7898d27f9b
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-04-28 11:31:26 -0300

    novo projeto

---
+++ HTML-JS/JS/script.js
@@ -0,0 +1,33 @@
+function OlaMundo() {
+  alert("Ola Mundo");
+}
+/* 
+  const OlaMundo = () => {
+    alert("Ola Mundo");
+  };
+*/
+
+const mudarTexto = () => {
+  // Criar uma variavel para armazenar o elemento do documento
+
+  let elementoAlvo = document.getElementById("texto");
+
+  elementoAlvo.innerHTML = "Novo Texto com javaScript";
+  elementoAlvo.style.color = "white";
+  elementoAlvo.style.backgroundColor = "black";
+};
+
+const enviarNome = () => {
+  let nomeDigitado = document.getElementById("nome").value;
+  alert("Olá " + nomeDigitado);
+
+  /*
+    let nomeDigitado = document.getElementsByName.value;
+    alert("ola" + nomeDigitado);
+  */
+
+};
+
+const somar = () => {
+  
+}
